988b773063513c6324a65e799247e17a6fa57516
Felipe Ballesteros <felipeballesteros1@gmail.com>
2017-10-19 22:50:15 -0700

    Library update

--- api/control.go
+++ api/control.go
@@ -1,16 +1,33 @@
 package api
 
-import "github.com/febg/Climbtracker/data"
+import (
+	"log"
+
+	"github.com/febg/Climbtracker/data"
+)
 
 // Control is
 type Control struct {
-	DataBase data.MySQL
+	Config   ControlConfig
+	DataBase data.MySQLDB
 }
 
+// ControlConfig configures the settings of the server controller
 type ControlConfig struct {
 	LocalHost bool
 }
 
+// NewControl creates and initialzes a new server controller using conifg settings
 func NewControl(config ControlConfig) {
-
+	c := Control{
+		Config: config,
+	}
+	defer log.Printf("[LOG] Started server controller { [Localhost: %v] }", c.Config.LocalHost)
+	var err error
+	if config.LocalHost {
+		c.DataBase, err = data.NewMySQL()
+		if err != nil {
+			log.Printf("[ERROR] Could not ")
+		}
+	}
 }
