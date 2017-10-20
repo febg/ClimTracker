cfdf7f2e7da69133fb3b79f3e60a528a4e43af1a
Felipe Ballesteros <felipeballesteros1@gmail.com>
2017-10-19 23:51:24 -0700

    Library update

--- api/router.go
+++ api/router.go
@@ -6,7 +6,7 @@ import "github.com/gorilla/mux"
 func StandardRouter(c *Control) *mux.Router {
 	r := mux.NewRouter()
 
-	r.Methods("POST").Path("/register/{user_name}/{user_email}/{user_password}").HandlerFunc(c.PostRegisterUser)
+	r.Methods("POST").Path("/register/").HandlerFunc(c.PostRegisterUser)
 	r.Methods("POST").Path("/login/{user_emial}/{user_password}").HandlerFunc(c.PostLogInUser)
 
 	return r
