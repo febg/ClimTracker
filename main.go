0f24795709b40051e665f8401735c5adf8adc7b7
Felipe Ballesteros <felipeballesteros1@gmail.com>
2017-10-19 23:26:49 -0700

    Library update

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/febg/Climbtracker/api"
+)
+
 func main() {
 
+	c, err := api.NewControl(api.ControlConfig{
+		LocalHost: true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	router := api.StandardRouter(c)
+	log.Println("[INFO] Listening on http://localhost:8080")
+	err = http.ListenAndServe(":8080", router)
+	if err != nil {
+		log.Fatal("ListenAndServe Error: ", err)
+	}
 }
